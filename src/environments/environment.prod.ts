4ddd40d4b6b2acfa93adb47753597450bc833542
Philipp <p.pichler1@gmail.com>
2019-11-27 22:19:47 +0100

    update prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
+  seoTitle: 'Goove - Erlebe und entdecke Wien, jetzt NEU - als Web-App verfügbar',
+  seoTitlePostFix: 'now on goove, Vienna finest food app ',
+  seoDescription: 'Wir bringen das Beste, was Wien zu bieten hat - hübsch verpackt - auf dein Smartphone. Neue Bars, Restaurants und angesagte Partys findest du easy mit goove',
+  seoImageUrl: 'assets/icons/icon-192x192.png',
   production: true
 };

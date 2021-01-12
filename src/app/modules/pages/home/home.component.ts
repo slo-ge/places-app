dc08d6422505f488c3dbfea98d2103b5170cbf71
Philipp <p.pichler1@gmail.com>
2021-01-12 17:11:29 +0100

    some small refactorings

--- src/app/modules/pages/home/home.component.ts
+++ src/app/modules/pages/home/home.component.ts
@@ -3,6 +3,13 @@ import {FormControl, FormGroup} from "@angular/forms";
 import {AdapterService} from "@app/core/services/adapter.service";
 import {ApiAdapter} from "@app/core/model/content.service";
 
+export const PREDEFINED_TEST_URL: { name: string, url: string }[] = [
+  {name: 'Any Restaurant', url: 'https://goove.at/detail/pizza-senza-danza'},
+  {name: 'A WordPress Blog', url: 'https://www.les-nouveaux-riches.com'},
+  {name: 'Techcrunch Blog', url: 'https://techcrunch.com'},
+  {name: 'Wired Magazinze Article', url: 'https://www.wired.com/story/job-screening-service-halts-facial-analysis-applicants/'}
+];
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -18,6 +25,7 @@ export class HomeComponent {
     url: new FormControl(''),
   });
 
+  testUrls = PREDEFINED_TEST_URL;
   loading = false;
   adapter: ApiAdapter | null | false = null;
   ApiAdapter = ApiAdapter;
@@ -34,5 +42,10 @@ export class HomeComponent {
     this.loading = true;
     this.adapter = await this.adapterService.findAdapter(this.baseURLForm.get('url')?.value).catch(() => false);
     this.loading = false;
+  }
+
+  selectItem(item: { name: string; url: string }) {
+    this.baseURLForm.patchValue({url: item.url});
+    this.check();
   }
 }

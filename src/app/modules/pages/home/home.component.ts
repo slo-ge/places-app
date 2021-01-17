2f3b13e0670fc7a4f20fb6548ceb31448752bf9b
Philipp <p.pichler1@gmail.com>
2021-01-17 18:11:21 +0100

    add more test link resources

--- src/app/modules/pages/home/home.component.ts
+++ src/app/modules/pages/home/home.component.ts
@@ -6,8 +6,18 @@ import {ApiAdapter} from "@app/core/model/content.service";
 export const PREDEFINED_TEST_URL: { name: string, url: string }[] = [
   {name: 'Any Restaurant', url: 'https://goove.at/detail/pizza-senza-danza'},
   {name: 'A WordPress Blog', url: 'https://www.les-nouveaux-riches.com'},
+  {name: 'A Spotify Album', url: 'https://open.spotify.com/album/5jQuMxOb3r5BPmSDke93hy'},
+  {name: 'A Spotify Playlist', url: 'https://open.spotify.com/playlist/37i9dQZF1DXcTieYAg7jq1'},
+  {name: 'A Pinterest Entry', url: 'https://www.pinterest.at/pin/207376757830118362/'},
   {name: 'Techcrunch Blog', url: 'https://techcrunch.com'},
-  {name: 'Wired Magazinze Article', url: 'https://www.wired.com/story/job-screening-service-halts-facial-analysis-applicants/'}
+  {name: 'Wired Magazine Article', url: 'https://www.wired.com/story/job-screening-service-halts-facial-analysis-applicants/'},
+  {name: 'Medium  Article', url: 'https://medium.com/the-ascent/my-overly-simple-rules-for-a-good-life-16553b9987fd'},
+  {name: 'Booking.com hotel', url: 'https://www.willhaben.at/iad/gebrauchtwagen/d/auto/bmw-x5-437396452'},
+  {name: 'Etsy  Product', url: 'https://www.etsy.com/de/listing/241172375/kuschelsack-canicula-fur-hunde?ref=finds_l&external=1'},
+  {name: 'Ikea  Product', url: 'https://www.ikea.com/at/de/p/landskrona-3er-sofa-mit-recamiere-gunnared-dunkelgrau-holz-s09272666/'},
+  {name: 'Ebay  Product', url: 'https://www.ebay.com/itm/FUJIFILM-Fuji-X70-Black-115/373425780807?epid=16025329757&hash=item56f1e93847:g:KYYAAOSwC55f~U14'},
+  {name: 'Willhaben Car', url: 'https://www.willhaben.at/iad/gebrauchtwagen/d/auto/bmw-x5-437396452'},
+  {name: 'Deliveroo.co.uk Restaurant', url: 'https://deliveroo.co.uk/menu/london/lambeth/out-fry-hercules-road?day=today&postcode=SW1A2AA&time=ASAP'},
 ];
 
 @Component({

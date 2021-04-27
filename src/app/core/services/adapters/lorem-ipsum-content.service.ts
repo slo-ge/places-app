dd8879b121c50c968d6a7abadf51f301628946d8
Philipp <p.pichler1@gmail.com>
2021-04-27 20:02:32 +0200

    change lorem-ipsum image path

--- src/app/core/services/adapters/lorem-ipsum-content.service.ts
+++ src/app/core/services/adapters/lorem-ipsum-content.service.ts
@@ -15,7 +15,7 @@ export class LoremIpsumContentService implements EditorPreviewInfoService {
     return of({
       title: 'Heading: you can edit this text box',
       description: 'Welcome to the meta-mapper edit mode. By clicking on this text, you can change it, you can also change the font size, the color, or the font type. It\'s also possible to change or remove this image.' ,
-      image: 'https://www.meta-mapper.com/assets/images/lorem-ipsum.jpg',
+      image: 'https://www.phipluspi.com/wp-content/uploads/2021/04/lorem-ipsum.jpg',
       iconUrl: 'https://www.meta-mapper.com/assets/meta/android-icon-192x192.png',
       url: 'https://www.meta-mapper.com'
     });

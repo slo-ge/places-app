879c6762c156ee7a15fbfdfa60f6740c0bd201c2
Philipp <p.pichler1@gmail.com>
2021-05-01 11:02:00 +0200

    change seo title

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
     seoTitle: 'Goove - Erlebe und entdecke Wien, gemütliche Restaurants in deiner Nähe.',
-    seoSearchTitle: 'goove.at | best places in Vienna',
-    seoTitlePostFix: 'goove.at | Vienna finest food app',
+    seoSearchTitle: 'goove.at | Your Vienna City Guide',
+    seoTitlePostFix: 'goove.at | Your Vienna City Guide',
     seoDescription: 'Wir bringen das Beste, was Wien zu bieten hat - hübsch verpackt - auf dein Smartphone. Neue Bars, Restaurants und angesagte Partys findest du easy auf goove.at',
     seoImageUrl: 'assets/icons/icon-192x192.png',
     apiUrl: 'https://locations.phipluspi.com/wp-json/wp/v2',

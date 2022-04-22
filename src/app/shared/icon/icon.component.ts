15537fe258f9e0f9ec3cb7766cebc724b48bff99
Philipp <p.pichler1@gmail.com>
2022-04-22 15:42:03 +0200

    add trending widget

--- src/app/shared/icon/icon.component.ts
+++ src/app/shared/icon/icon.component.ts
@@ -1,22 +1,24 @@
 import {Component, Input} from '@angular/core';
 
 @Component({
-  selector: 'app-icon',
-  templateUrl: './icon.component.html',
-  styleUrls: ['./icon.component.scss']
+    selector: 'app-icon',
+    templateUrl: './icon.component.html',
+    styleUrls: ['./icon.component.scss']
 })
 export class IconComponent {
 
-  @Input()
-  name: 'back'
-      | 'geo-search'
-      | 'sort-latest'
-      | 'italic-burger-menu'
-      | 'whatsapp'
-      | 'telegram'
-      | 'yummy'
-      | 'search'
-      | 'www';
+    @Input()
+    name: 'back'
+        | 'geo-search'
+        | 'sort-latest'
+        | 'italic-burger-menu'
+        | 'whatsapp'
+        | 'telegram'
+        | 'yummy'
+        | 'search'
+        | 'www'
+        | 'trending';
 
-  constructor() { }
+    constructor() {
+    }
 }

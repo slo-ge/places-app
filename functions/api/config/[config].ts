b7548c157220733ee08db00d7ece14f900a1e0c5
philipp <p.pichler1@gmail.com>
2023-08-29 19:19:35 +0200

    simple referrer handling in cloudflare

--- functions/api/config/[config].ts
+++ functions/api/config/[config].ts
@@ -1,7 +1,14 @@
-import {APP_CONFIGS} from "../../../lib/config";
+import { APP_CONFIGS, APP_FALLBACK_CONFIG } from '../../../lib/config';
 
 export async function onRequest(event: EventContext<any, any, any>) {
   const configName = event.params.config;
+  console.log(event.request.url);
+
+  const url = new URL(event.request.url);
+  if (url.searchParams?.get('referrer') === 'meta-mapper.com'){
+      return APP_FALLBACK_CONFIG;
+  }
+
   const config = APP_CONFIGS.get(configName as string) || APP_CONFIGS.get('default');
   return new Response(JSON.stringify(config), {headers: {'content-type': 'application-json'}});
 }
--- src/app/core/services/initial-config.service.ts
+++ src/app/core/services/initial-config.service.ts
@@ -1,26 +1,27 @@
 import { Injectable } from '@angular/core';
-import { APP_FALLBACK_CONFIG, ApplicationConfig, FeatureFlags } from "@shared-lib/config";
-import { CmsAuthService } from "@app/core/services/cms-auth.service";
-import { Observable, ReplaySubject } from "rxjs";
+import { APP_FALLBACK_CONFIG, ApplicationConfig } from '@shared-lib/config';
+import { CmsAuthService } from '@app/core/services/cms-auth.service';
+import { Observable, ReplaySubject } from 'rxjs';
 
 @Injectable({
-    providedIn: 'root'
+    providedIn: 'root',
 })
 export class InitialConfigService {
     private initialConfig!: ApplicationConfig;
     private currentConfig$: ReplaySubject<ApplicationConfig> = new ReplaySubject<ApplicationConfig>();
 
-    constructor(private authService: CmsAuthService) {
-    }
+    constructor(private authService: CmsAuthService) {}
 
     // Call me only once in modules
     async _initialConfigListener() {
+        const url = new URL(window.location.href);
+        const referrer = url.host;
         this.authService.getUser().subscribe(async user => {
             const username = user?.user.username || 'default';
             try {
-                const res = await fetch('/api/config/' + username);
+                const res = await fetch('/api/config/' + username + `?referrer=${referrer}`);
                 if (res.ok) {
-                    this.initialConfig = await res.json() as ApplicationConfig;
+                    this.initialConfig = (await res.json()) as ApplicationConfig;
                 } else {
                     this.initialConfig = APP_FALLBACK_CONFIG;
                 }

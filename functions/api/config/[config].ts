ff02f9f528e64a0f5e5d4c26484d61586d667f69
philipp <p.pichler1@gmail.com>
2023-08-29 20:00:30 +0200

    fix pfunction

--- functions/api/config/[config].ts
+++ functions/api/config/[config].ts
@@ -1,16 +1,16 @@
 import { APP_CONFIGS, APP_FALLBACK_CONFIG } from '../../../lib/config';
 
 export async function onRequest(event: EventContext<any, any, any>) {
-  const configName = event.params.config;
-  console.log(event.request.url);
+    const configName = event.params.config;
+    console.log(event.request.url);
 
-  const url = new URL(event.request.url);
-  const referrer = url.searchParams?.get('referrer');
+    const url = new URL(event.request.url);
+    const referrer = url.searchParams?.get('referrer');
 
-  if (['meta-mapper.com', 'www.meta-mapper.com'].includes(referrer)){
-      return APP_FALLBACK_CONFIG;
-  }
+    if (['meta-mapper.com', 'www.meta-mapper.com'].includes(referrer)) {
+        return new Response(JSON.stringify(APP_FALLBACK_CONFIG), { headers: { 'content-type': 'application-json' } });
+    }
 
-  const config = APP_CONFIGS.get(configName as string) || APP_CONFIGS.get('default');
-  return new Response(JSON.stringify(config), {headers: {'content-type': 'application-json'}});
+    const config = APP_CONFIGS.get(configName as string) || APP_CONFIGS.get('default');
+    return new Response(JSON.stringify(config), { headers: { 'content-type': 'application-json' } });
 }

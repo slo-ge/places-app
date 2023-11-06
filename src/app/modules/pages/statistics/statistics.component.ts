8ac73781d8f1add588f074d9d87fbb85346e98ba
philipp <p.pichler1@gmail.com>
2023-11-06 16:19:45 +0100

    hacky data for statistics page

--- src/app/modules/pages/statistics/statistics.component.ts
+++ src/app/modules/pages/statistics/statistics.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpTrackingService, TrackingEvent } from '@app/core/editor/http-tracking.service';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-
+import { map, tap } from 'rxjs/operators';
 
 function isJsonString(str: string) {
     try {
@@ -19,7 +18,7 @@ function isJsonString(str: string) {
     styleUrls: ['./statistics.component.scss'],
 })
 export class StatisticsComponent {
-    readonly trackingEvents$: Observable<Map<string, Array<TrackingEvent>>>;
+    readonly trackingEvents$: Observable<Array<TrackingEvent & { count: number; date: Date }>>;
 
     constructor(httpTrackingService: HttpTrackingService) {
         this.trackingEvents$ = httpTrackingService.getAllTrackingEvents().pipe(
@@ -31,14 +30,43 @@ export class StatisticsComponent {
                     }
 
                     const date = new Date(event.eventDate);
-                    const key = `${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()}`;
+                    const key = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
                     if (map.get(key)) {
                         map.set(key, [...map.get(key)!, event]);
                     } else {
                         map.set(key, [event]);
                     }
                 }
+
                 return map;
+            }),
+            map(data => {
+                const aggregatedEvents = Array<TrackingEvent  & { count: number; date: Date }>();
+                for (const [_key, value] of data.entries()) {
+                    const tmpValue = value.reduce((acc, obj) => {
+                        const key = obj.trackingDetail; // Create a unique key for each object
+                        if (!acc.has(key)) {
+                            acc.set(key, { ...obj, count: 1 });
+                        } else {
+                            const existingObj = acc.get(key);
+                            if (existingObj) {
+                                existingObj.count++;
+                            }
+                        }
+
+                        return acc;
+                    }, new Map<string, TrackingEvent & { count: number }>());
+
+                    const parts = _key.split("-"); // Split the string into parts based on the hyphen
+                    const day = parseInt(parts[0], 10);
+                    const month = parseInt(parts[1], 10) - 1; // Subtract 1 from the month to match the zero-based index
+                    const year = parseInt(parts[2], 10);
+                    const parsedDate = new Date(year, month, day);
+
+                    aggregatedEvents.push({...Array.from(tmpValue.values())[0], date: parsedDate});
+                }
+                const sortedEvents = [...aggregatedEvents].sort((a, b) => a.date.getTime() - b.date.getTime());
+                return sortedEvents;
             })
         );
     }
